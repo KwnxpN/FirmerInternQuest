096cc79b5877f01970d9b65e66b0e6f2efd7cd81
KwnxpN <tonnum.kawin@gmail.com>
2026-02-03 12:17:16 +0700

    feat: add get all users endpoint

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,10 @@
+import User from '../models/User.js';
+
+export async function getAllUsers(req, res) {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import logsRoutes from './routes/logsRoutes.js';
+import usersRoutes from './routes/usersRoutes.js';
 import { connectDB } from './config/db.js';
 import './models/index.js';
 
@@ -15,8 +16,7 @@ connectDB();
 app.use(express.json());
 
 app.use('/api/logs', logsRoutes);
-
-
+app.use('/api/users', usersRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
