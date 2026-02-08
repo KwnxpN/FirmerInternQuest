cc064183229067e75480a7e8f526099bb889dcd6
KwnxpN <tonnum.kawin@gmail.com>
2026-02-09 01:05:49 +0700

    fix: typo

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectDB();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: process.env.CORS_ORING_LOCAL_NGINX,
+  origin: process.env.CORS_ORIGIN_LOCAL_NGINX,
   origin: process.env.CORS_ORIGIN_LOCAL,
   origin: process.env.CORS_ORIGIN,
   credentials: true,
