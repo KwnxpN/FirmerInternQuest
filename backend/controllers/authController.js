6c49a19fa05caebe21de6e4c39658b809cb8fabd
KwnxpN <tonnum.kawin@gmail.com>
2026-02-09 01:12:00 +0700

    feat: set cookie for https and cross site

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -30,8 +30,8 @@ export async function login(req, res) {
 
     res.cookie('token', token, {
         httpOnly: true,
-        secure: false,
-        sameSite: 'lax',
+        secure: true,
+        sameSite: 'None',
         maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
